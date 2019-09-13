957bdad2e88b54aa7f9135094ebf461bff28fb29
junxiang <junxiang.oriint@gmail.com>
2019-09-14 01:45:47 +0800

    update: index.php  - UI and logout button

--- public/backoffice/index.php
+++ public/backoffice/index.php
@@ -12,7 +12,7 @@ if (!empty($_SESSION["su"])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Attendees</title>
     <link
       rel="stylesheet"
       href="https://use.fontawesome.com/releases/v5.7.1/css/all.css"
@@ -29,8 +29,58 @@ if (!empty($_SESSION["su"])) {
 </head>
 <body>
     <div id="app" style="display: none;" v-show="true">
-        <?php echo $su; ?>
-        <a href="/api/postLogout.php" class="logout-button">Logout</a>
+        <el-container>
+            <el-header>
+                <!-- <?php echo $su; ?> -->
+                <el-row :gutter="20">
+                    <el-col :span="2">
+                        <el-avatar shape="square"> GVM </el-avatar>
+                    </el-col>
+                    <el-col :span="12">
+                        <el-input
+                            placeholder="全文檢索"
+                            prefix-icon="el-icon-search"
+                            >
+                        </el-input>
+                    </el-col>
+                    <el-col :span="10" style="text-align: right;">
+                        <el-button type="success" icon="el-icon-download" circle></el-button>
+                        <el-button type="danger" icon="fas fa-sign-out-alt" circle @click.prevent="logout"></el-button>
+                    </el-col>
+                </el-row>
+
+            </el-header>
+            <el-main>
+                <el-card class="box-card">
+                    <el-table
+                        :data="tableData"
+                        style="width: 100%">
+                        <el-table-column
+                            sortable
+                            prop="date"
+                            label="日期"
+                            width="180">
+                        </el-table-column>
+                        <el-table-column
+                            prop="name"
+                            label="姓名"
+                            width="180">
+                        </el-table-column>
+                        <el-table-column
+                            prop="address"
+                            label="地址">
+                        </el-table-column>
+                    </el-table>
+                    <el-pagination
+                        background
+                        layout="prev, pager, next"
+                        :page-size="10"
+                        :total="100">
+                    </el-pagination>
+                </el-card>
+            </el-main>
+        </el-container>
+
     </div>
 </body>
 <script src="./js/index.js"></script>
--- public/backoffice/js/index.js
+++ public/backoffice/js/index.js
@@ -4,17 +4,51 @@ var Index = {
 		Index.instanceVue = new Vue({
 			el: '#app',
 			data: {
-				
+				tableData: [
+					{
+						date: '2016-05-03',
+						name: '王小虎',
+						address: '上海市普陀区金沙江路 1518 弄',
+					},
+					{
+						date: '2016-05-02',
+						name: '王小虎',
+						address: '上海市普陀区金沙江路 1518 弄',
+					},
+					{
+						date: '2016-05-04',
+						name: '王小虎',
+						address: '上海市普陀区金沙江路 1518 弄',
+					},
+					{
+						date: '2016-05-01',
+						name: '王小虎',
+						address: '上海市普陀区金沙江路 1518 弄',
+					},
+					{
+						date: '2016-05-08',
+						name: '王小虎',
+						address: '上海市普陀区金沙江路 1518 弄',
+					},
+					{
+						date: '2016-05-06',
+						name: '王小虎',
+						address: '上海市普陀区金沙江路 1518 弄',
+					},
+					{
+						date: '2016-05-07',
+						name: '王小虎',
+						address: '上海市普陀区金沙江路 1518 弄',
+					}
+				],
 			},
-			
+
 			created: function() {},
 			mounted: function() {
-                console.log(123)
-            },
+			},
 			methods: {
-				
-				clickEstimate: function() {
-					console.log('Ping Pong ~');
+				logout: function() {
+					window.location = '/api/postLogout.php';
 				},
 			},
 		});
