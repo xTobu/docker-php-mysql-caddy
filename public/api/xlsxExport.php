8e864259354176681bea257b8959e7e1c0b28741
junxiang <junxiang.oriint@gmail.com>
2019-09-16 11:22:14 +0800

    update: xlsxExport.php

--- public/api/xlsxExport.php
+++ public/api/xlsxExport.php
@@ -72,19 +72,12 @@ try {
     exit();
 }
 
-$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
-ob_end_clean();
 
-// Redirect output to a client’s web browser (Excel2007)
-header('Content-type: application/vnd.ms-excel');
+header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
 header('Content-Disposition: attachment;filename="GVM_Event_報名名單.xlsx"');
 header('Cache-Control: max-age=0');
-// If you're serving to IE 9, then the following may be needed
-header('Cache-Control: max-age=1');
 
-// If you're serving to IE over SSL, then the following may be needed
-header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT'); // always modified
-header('Cache-Control: cache, must-revalidate'); // HTTP/1.1
-header('Pragma: public'); // HTTP/1.0
+$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+ob_end_clean();
 
 $objWriter->save('php://output');
