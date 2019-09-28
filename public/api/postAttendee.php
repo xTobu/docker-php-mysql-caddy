7d256439599defdf866e036cc1996bf47cc09deb
junxiang <junxiang.oriint@gmail.com>
2019-09-28 09:02:47 +0800

    update: check duplicate data

--- public/api/postAttendee.php
+++ public/api/postAttendee.php
@@ -17,26 +17,40 @@ try {
     $phone = $_POST['phone'];
     $email = $_POST['email'];
 
-    if (isset($event, $session, $job, $dept, $name, $phone, $email)) {
-
-        $query = "INSERT INTO `attendee` (`event`, `session`, `job`, `dept`, `rocid`, `name`, `phone`, `email`) VALUES (:event, :session, :job, :dept, :rocid, :name, :phone, :email)";
-        $statement = $conn->prepare($query);
-
-        $statement->bindParam(':event', $event);
-        $statement->bindParam(':session', $session);
-        $statement->bindParam(':job', $job);
-        $statement->bindParam(':dept', $dept);
-        $statement->bindParam(':rocid', $rocid ?: null);
-        $statement->bindParam(':name', $name);
-        $statement->bindParam(':phone', $phone);
-        $statement->bindParam(':email', $email);
-
-        $statement->execute();
-
-        $handler->response(200, "Success");
-    } else {
-        $handler->response(400, "Incomplete Request");
+    if (!isset($event, $session, $job, $dept, $name, $phone, $email)) {
+        $handler->response(400, "Bad Request: Incomplete Request");
+        exit();
     }
+
+    $query = "SELECT COUNT(*) FROM `attendee` WHERE `event` = :event AND `session` = :session AND `email` = :email";
+    $statement = $conn->prepare($query);
+    $statement->bindParam(':event', $event);
+    $statement->bindParam(':session', $session);
+    $statement->bindParam(':email', $email);
+    $statement->execute();
+    
+    // 檢查重複資料
+    $count = $statement->fetchColumn();
+    if($count > 0){
+        $handler->response(409, "Conflict: Duplicate Data");
+        exit();
+    }
+    
+    $query = "INSERT INTO `attendee` (`event`, `session`, `job`, `dept`, `rocid`, `name`, `phone`, `email`) VALUES (:event, :session, :job, :dept, :rocid, :name, :phone, :email)";
+    $statement = $conn->prepare($query);
+
+    $statement->bindParam(':event', $event);
+    $statement->bindParam(':session', $session);
+    $statement->bindParam(':job', $job);
+    $statement->bindParam(':dept', $dept);
+    $statement->bindParam(':rocid', $rocid ?: null);
+    $statement->bindParam(':name', $name);
+    $statement->bindParam(':phone', $phone);
+    $statement->bindParam(':email', $email);
+
+    $statement->execute();
+
+    $handler->response(200, "Success");
 } catch (PDOException $ex) {
     $handler->response(500, $ex->getMessage());
 }
