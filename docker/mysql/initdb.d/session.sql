25094c8282150597a0b4599a3030bb132d78c0ca
junxiang <junxiang.oriint@gmail.com>
2019-11-30 16:57:43 +0800

    add: session.sql

---
+++ docker/mysql/initdb.d/session.sql
@@ -0,0 +1,28 @@
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+CREATE DATABASE IF NOT EXISTS `db_Event`;
+USE db_Event;
+
+--
+-- Table structure for table `session`
+--
+
+create table if not exists `session`
+(
+    `pkid`       int auto_increment                 primary key,
+    `session`    varchar(255)                       null,
+    `limit`      int                                null,
+    `status`     int      default 1                 null,
+    `deleted_at` datetime                           null,
+    `updated_at` datetime    on update current_timestamp,
+    `created_at` datetime      default current_timestamp
+);
+
+
+-- ----------------------------
+-- Records of session
+-- ----------------------------
+BEGIN;
+INSERT INTO `session` (`session`) VALUES ("總論壇");
+COMMIT;
